ea313baa4debc31f05ce5ca2a1bcfd617bd1928b
gabrielbrotzmann <gabrielbrotzmann@live.com.ar>
2021-03-24 20:16:40 -0300

    Agrego ejercicio 2

---
+++ EjCSharp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjCSharp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ingrese un texto por teclado, luego presione enter para finalizar");
+
+            string texto = Console.ReadLine();
+
+            Console.WriteLine(texto);
+
+            Console.ReadKey();
+
+        }
+    }
+}
